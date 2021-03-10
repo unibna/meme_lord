d01773531889d9a7ea03281956375480a786a670
unibna <nguyenduylan711@gmail.com>
2021-03-10 15:50:04 +0700

    Deploy on Heroku

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.SearchView.as_view(), name='search'),
-    path('home/',  views.HomeView.as_view(), name='home')
+    path('/',  views.HomeView.as_view(), name='home')
 ]
